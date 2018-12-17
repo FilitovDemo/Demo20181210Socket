27e7849e73c130ade65c704b4a0d2e5a3a56b546
FilitovDemo <filitov@twcat.net>
2018-12-17 12:06:57 +0800

    Message and Handler

--- app/src/main/java/tw/edu/tut/mis/demo20181210socket/MainActivity.java
+++ app/src/main/java/tw/edu/tut/mis/demo20181210socket/MainActivity.java
@@ -1,10 +1,16 @@
 package tw.edu.tut.mis.demo20181210socket;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -12,13 +18,22 @@ import java.net.SocketException;
 
 public class MainActivity extends AppCompatActivity {
 
+    UDPReceiveThread xxx;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        UDPReceiveThread xxx = new UDPReceiveThread();
+        xxx = new UDPReceiveThread();
         xxx.start();
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xxx.getHandler().obtainMessage(1097).sendToTarget();
+            }
+        });
 
         UDPSendRunnable ooo = new UDPSendRunnable();
         ooo.IP = "192.168.64.255";
@@ -27,13 +42,57 @@ public class MainActivity extends AppCompatActivity {
         new Thread(ooo).start();
     }
 
+    //
+    static class MainActivityHandler extends Handler {
+        //----------------------------------------
+        private WeakReference<MainActivity> activity;
+
+        MainActivityHandler(MainActivity activity) {
+            this.activity = new WeakReference<>(activity);
+        }
+        //----------------------------------------
+        @Override
+        public void handleMessage(Message msg) {
+            MainActivity mainActivity = activity.get();
+
+            if (msg.what==1988) { //編號是1988要做的事情
+                //留意，要透過 mainActivity 取用 MainActivity的東西
+                String s = (String)(msg.obj);
+                ((TextView)(mainActivity.findViewById(R.id.textView))).setText(s);
+            }
+        }
+    }
+
+    Handler mMainHandler = new MainActivityHandler(this);
+
+
+
+
+
     //android要求網路傳輸不能在主要執行緒中
     //建立一個用來接收UDP封包的執行緒
     class UDPReceiveThread extends Thread {
+        Handler mReceiveHandler;
         boolean keepLoop = true;
+
+        Handler getHandler(){
+            return mReceiveHandler;
+        }
+
         @Override
         public void run() {
             super.run();
+
+            Looper.prepare();
+            mReceiveHandler = new Handler(){
+                @Override
+                public void handleMessage(Message msg) {
+                    if (msg.what==1097) {  //如果收到 1097 號
+                        keepLoop = false;
+                    }
+                }
+            };
+            Looper.loop();
 
             try( DatagramSocket socket = new DatagramSocket(9527) ){
                 byte[] buffer = new byte[1024]; //大小自己訂
@@ -43,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
                     socket.receive(packet);  //阻塞式等待
                     String s = new String(packet.getData(), 0, packet.getLength());
                     Log.d("DEMO UDP", s);
+                    mMainHandler.obtainMessage(1988, s).sendToTarget();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
